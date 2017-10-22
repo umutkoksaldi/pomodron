5ae7ee3f6aac00cccb56350df8b48982bc8ba447
Umut Köksaldı <umut.4182@gmail.com>
2017-10-22 17:15:29 +0300

    Basic logic setup

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+//SET ENV
+process.env.NODE_ENV = 'production';
 
 let mainWindow;
+let addWindow;
 
 // Listen for the app to be ready
 app.on('ready', function() {
@@ -16,11 +20,41 @@ app.on('ready', function() {
         protocol: 'file:',
         slashes: true
     }));
+    // Quit app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
 
     // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // Insert menu
     Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+function createAddWindow() {
+    // Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add shopping list item'
+    });
+    // Load main html file
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+}
+
+// Catch item:add
+ipcMain.on('item:add', function(e, item){
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
 });
 
 // Create menu template
@@ -29,8 +63,48 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
             }
         ]
     }
 ];
+
+// If mac, add empty object to menu
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+if (process.env.NODE_ENV != 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle Dev Tools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+    ]
+    })
+}
